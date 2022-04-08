5a6fc8f736621a1409c978671d8e5ae13f880fdf
lunar <mo0nlight0@naver.com>
2022-04-08 17:22:08 +0900

    feat: sort by category function add.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.categories_page)
+
 ]
